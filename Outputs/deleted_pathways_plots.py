e4a0caf49e02dc00502b8bb424873fb7b6d1d596
am1520x <am1520@ic.ac.uk>
2025-06-09 15:38:39 +0100

    Added the saved plot and updated the file path logic.

--- Outputs/deleted_pathways_plots.py
+++ Outputs/deleted_pathways_plots.py
@@ -8,7 +8,7 @@ import traceback
 from glob import glob
 from collections import defaultdict
 
-DATA_DIR = "/content/"  # path to directory containing all pumpkin_output_cell_*.txt files
+DATA_DIR = ""  # path to directory containing all pumpkin_output_cell_*.txt files
 N_CELLS = 175
 
 deleted_contributions = defaultdict(lambda: {"production": [np.nan] * N_CELLS, "consumption": [np.nan] * N_CELLS})
